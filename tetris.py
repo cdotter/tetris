2a7a1ba2974a90e33ae0c2983aeaa16a71ebc469
cdotter <cdotter@gmail.com>
2016-01-29 23:51:52 -0500

    n/a

--- tetris.py
+++ tetris.py
@@ -183,8 +183,8 @@ for i in range(0,42):
         print("\n")
 
 '''
-    pass other working coord sets (dropped_coords, rotated_coords, slid_coords) to collision_check()
-    DON'T append coords if collision is found for rotate/slide AND the coord still has room to fall
+    Pass other working coord sets (dropped_coords, rotated_coords, slid_coords) to collision_check()
+    DON'T append coords if collision is found for rotate/slide AND the coord still has room to fall;
         instead, disallow rotate/slide but let it continue to fall until there is no more room
     
     Flatten game_pieces by default so no need for reduced_game_pieces in row_clear_check().
